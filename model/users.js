7668b8d63aecee308f7d86399855f9fc88a02b38
meilissalee13 <meilissa@node8.net>
2016-11-27 01:12:01 +0700

    beres dong

--- model/users.js
+++ model/users.js
@@ -12,8 +12,6 @@ function user(router,connection,md5) {
 user.prototype.handleRoutes = function(router,connection,md5) {
     var self = this;
 
-    //there's deleted code in here
-
     //push notif ios
     router.post("/pushNotif",function(req,res){
 

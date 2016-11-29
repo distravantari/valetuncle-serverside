749d8a835d36db17ce083bed08aaf1996423c13a
meilissalee13 <meilissa@node8.net>
2016-11-29 16:01:06 +0700

    rollback test tele 4

--- model/users.js
+++ model/users.js
@@ -475,140 +475,140 @@ user.prototype.handleRoutes = function(router,connection,md5) {
     });
 
 //findNearestDriver(transId)
-    // router.post("/findNearestDriver",function(req,res){
-    //     var transId = req.body.transId;
-    //     var range = 0;
-    //     connection.query("SELECT `range` FROM `setting` WHERE objectId = 0",function(err,jarak){
-    //       if(err){
-    //         res.json({"Error" : true, "Message" : "err.."});
-    //       }else{
-    //         range = jarak[0].range;
-    //         connection.query("SELECT username,driverTemp,pickUp,whosDecline from transaction WHERE objectId= "+transId,function(err,rows){
-    //           if(err){
-    //               res.json({"message":"err.."});
-    //           }else {
-    //               if(rows.length>=1)
-    //               {
-    //                   if ((rows[0].driverTemp != null)&&(rows[0].driverTemp != "")&&(rows[0].driverTemp != "no")&&(rows[0].driverTemp != "u")) {
-    //                       //res.json("no transaction available");
-    //                       res.json({"message":"no transaction available"});
-    //                   } else{
-    //                       // we are using kilometers
-    //                       var inKilometers = 0;
-    //                         //connection.query("SELECT flag,status,location,objectId from driver where flag = 1 and status = 1 Order by RAND()",function(err,queryDriver){
-    //                         connection.query("SELECT flag,status,location,objectId from driver where flag = 1 and status = 1 Order by priority ASC,RAND()",function(err,queryDriver){
-    //                         //connection.query("SELECT flag,status,location,objectId from driver where flag = 1 and status = 1 and objectId IN (SELECT driverId FROM transaction where status = 'advance' AND (advanceTime > NOW() + INTERVAL 2 HOUR)) Order by RAND()",function(err,queryDriver){
-    //                           if(err){
-    //                               res.json({"message":"error"});
-    //                           }else{
-    //                               //located where user location is
-    //                               var locTemp = rows[0].pickUp.split(";");
-    //                               var latitude = Number(locTemp[0]);
-    //                               var longitude = Number(locTemp[1]);
-    //                               var userGeoPoint = new GeoPoint(latitude,longitude); //success
-    //   //                            res.json({"message":latitude});
-
-    //                               // only for driver within 5km
-    //                               var driverNearby = "";
-    //                               for (var i = 0; i < queryDriver.length; i++) {
-    //                                   //located where driver location is
-    //                                   var getLocation=queryDriver[i].location
-    //                                   if((getLocation!="")&&(getLocation!=null)&&(getLocation!=undefined))
-    //                                   {
-    //                                       locTemp = getLocation.split(";");
-    //                                       var latD = Number(locTemp[0]);
-    //                                       var longD = Number(locTemp[1]);
-    //                                       var driverGeoPoint = new GeoPoint(latD,longD);
-
-    //                                       //check the distance in kilo
-    //                                       inKilometers = driverGeoPoint.distanceTo(userGeoPoint, true);
-    //                                       // if distance <= 5KM then it will be push to an array
-    //                                         if(inKilometers <= range){
-    //                                             driverNearby+=queryDriver[i].objectId+";";
-    //                                         }
-    //                                   }
-    //                               };
-
-    //                               // is the driver banned ?
-    //                               var banned,driver;
-    //                               var notbanned = [""];
-    //                               var whosDecline = rows[0].whosDecline;
-    //                               //split whosdecline and driver nearby
-    //                               driver = driverNearby.split(";");
-    //                               // check whosDecline , if there is banned driver
-    //                               if ((whosDecline != null)&&(whosDecline != "")&&(whosDecline != undefined)) {
-
-    //                                   banned = whosDecline.split(";");
-
-    //                                   j=0;
-    //                                   for (var i=0; i < driver.length; ++i){
-    //                                       if (banned.indexOf(driver[i]) == -1){
-    //                                           // insert no banned driver to an array (notbanned)
-    //                                           notbanned[j++] = driver[i];
-    //                                       };
-    //                                   };
-
-    //                               }
-    //                               // if there is no driver banned
-    //                               else{
-    //                                   // insert all driver to not banned array
-    //                                   notbanned = driver;
-    //                               }
-
-    //                               if(notbanned.length==1&&notbanned[0]==""){
-    //                                   res.json({"message":"no driver around you "});
-    //                               }else{
-    //                                   var query = "UPDATE transaction SET driverTemp = ? WHERE objectId = "+transId;
-    //                                   var table = [notbanned[0]];
-    //                                   query = mysql.format(query,table);
-    //                                   connection.query(query,function(err,shh){
-    //                                       if(err){
-    //                                           res.json({"message":"err.. "+query});
-    //                                       }else{
-    //                                         //   res.json({"message":"success "});
-    //                                          // DATA MINING
-    //                                             var cancel_mining = "INSERT INTO action_log(username,driverId,action) VALUES (?,?,?)";
-    //                                             var table = [rows[0].username,"","user request"];
-    //                                             cancel_mining = mysql.format(cancel_mining,table);
-    //                                             connection.query(cancel_mining,function(err,suc){
-    //                                               if (err) {
-    //                                                 res.json({"message":"3 "+err});
-    //                                               }else{
-    //                                                 res.json({"message":"success "}); //dont change the resnponse
-    //                                               }
-    //                                             })
-    //                                       }
-    //                                   });
-    //                               };
-
-    //                           }
-    //                       });
-    //                   }
-    //               }
-    //               else
-    //               {
-    //                   res.json({"message":"no transaction available"});
-    //               }
-    //           }
-    //       });
-    //       }
-    //     })
-    // });
-
     router.post("/findNearestDriver",function(req,res){
-           var transId = req.body.transId;
-           var query = "UPDATE transaction SET driverTemp = '2', driverId = '2' WHERE objectId = "+transId;
-           query = mysql.format(query);
-           connection.query(query,function(err,shh){
-               if(err){
-                   console.log("err",err)
-                   res.json({"message":"no driver around you "});
-               }
-               else {
-                   res.json({"message":"success "}); //dont change the resnponse
-               }
-           })
+        var transId = req.body.transId;
+        var range = 0;
+        connection.query("SELECT `range` FROM `setting` WHERE objectId = 0",function(err,jarak){
+          if(err){
+            res.json({"Error" : true, "Message" : "err.."});
+          }else{
+            range = jarak[0].range;
+            connection.query("SELECT username,driverTemp,pickUp,whosDecline from transaction WHERE objectId= "+transId,function(err,rows){
+              if(err){
+                  res.json({"message":"err.."});
+              }else {
+                  if(rows.length>=1)
+                  {
+                      if ((rows[0].driverTemp != null)&&(rows[0].driverTemp != "")&&(rows[0].driverTemp != "no")&&(rows[0].driverTemp != "u")) {
+                          //res.json("no transaction available");
+                          res.json({"message":"no transaction available"});
+                      } else{
+                          // we are using kilometers
+                          var inKilometers = 0;
+                            //connection.query("SELECT flag,status,location,objectId from driver where flag = 1 and status = 1 Order by RAND()",function(err,queryDriver){
+                            connection.query("SELECT flag,status,location,objectId from driver where flag = 1 and status = 1 Order by priority ASC,RAND()",function(err,queryDriver){
+                            //connection.query("SELECT flag,status,location,objectId from driver where flag = 1 and status = 1 and objectId IN (SELECT driverId FROM transaction where status = 'advance' AND (advanceTime > NOW() + INTERVAL 2 HOUR)) Order by RAND()",function(err,queryDriver){
+                              if(err){
+                                  res.json({"message":"error"});
+                              }else{
+                                  //located where user location is
+                                  var locTemp = rows[0].pickUp.split(";");
+                                  var latitude = Number(locTemp[0]);
+                                  var longitude = Number(locTemp[1]);
+                                  var userGeoPoint = new GeoPoint(latitude,longitude); //success
+      //                            res.json({"message":latitude});
+
+                                  // only for driver within 5km
+                                  var driverNearby = "";
+                                  for (var i = 0; i < queryDriver.length; i++) {
+                                      //located where driver location is
+                                      var getLocation=queryDriver[i].location
+                                      if((getLocation!="")&&(getLocation!=null)&&(getLocation!=undefined))
+                                      {
+                                          locTemp = getLocation.split(";");
+                                          var latD = Number(locTemp[0]);
+                                          var longD = Number(locTemp[1]);
+                                          var driverGeoPoint = new GeoPoint(latD,longD);
+
+                                          //check the distance in kilo
+                                          inKilometers = driverGeoPoint.distanceTo(userGeoPoint, true);
+                                          // if distance <= 5KM then it will be push to an array
+                                            if(inKilometers <= range){
+                                                driverNearby+=queryDriver[i].objectId+";";
+                                            }
+                                      }
+                                  };
+
+                                  // is the driver banned ?
+                                  var banned,driver;
+                                  var notbanned = [""];
+                                  var whosDecline = rows[0].whosDecline;
+                                  //split whosdecline and driver nearby
+                                  driver = driverNearby.split(";");
+                                  // check whosDecline , if there is banned driver
+                                  if ((whosDecline != null)&&(whosDecline != "")&&(whosDecline != undefined)) {
+
+                                      banned = whosDecline.split(";");
+
+                                      j=0;
+                                      for (var i=0; i < driver.length; ++i){
+                                          if (banned.indexOf(driver[i]) == -1){
+                                              // insert no banned driver to an array (notbanned)
+                                              notbanned[j++] = driver[i];
+                                          };
+                                      };
+
+                                  }
+                                  // if there is no driver banned
+                                  else{
+                                      // insert all driver to not banned array
+                                      notbanned = driver;
+                                  }
+
+                                  if(notbanned.length==1&&notbanned[0]==""){
+                                      res.json({"message":"no driver around you "});
+                                  }else{
+                                      var query = "UPDATE transaction SET driverTemp = ? WHERE objectId = "+transId;
+                                      var table = [notbanned[0]];
+                                      query = mysql.format(query,table);
+                                      connection.query(query,function(err,shh){
+                                          if(err){
+                                              res.json({"message":"err.. "+query});
+                                          }else{
+                                            //   res.json({"message":"success "});
+                                             // DATA MINING
+                                                var cancel_mining = "INSERT INTO action_log(username,driverId,action) VALUES (?,?,?)";
+                                                var table = [rows[0].username,"","user request"];
+                                                cancel_mining = mysql.format(cancel_mining,table);
+                                                connection.query(cancel_mining,function(err,suc){
+                                                  if (err) {
+                                                    res.json({"message":"3 "+err});
+                                                  }else{
+                                                    res.json({"message":"success "}); //dont change the resnponse
+                                                  }
+                                                })
+                                          }
+                                      });
+                                  };
+
+                              }
+                          });
+                      }
+                  }
+                  else
+                  {
+                      res.json({"message":"no transaction available"});
+                  }
+              }
+          });
+          }
+        })
     });
+
+    // router.post("/findNearestDriver",function(req,res){
+    //        var transId = req.body.transId;
+    //        var query = "UPDATE transaction SET driverTemp = '2', driverId = '2' WHERE objectId = "+transId;
+    //        query = mysql.format(query);
+    //        connection.query(query,function(err,shh){
+    //            if(err){
+    //                console.log("err",err)
+    //                res.json({"message":"no driver around you "});
+    //            }
+    //            else {
+    //                res.json({"message":"success "}); //dont change the resnponse
+    //            }
+    //        })
+    // });
 
     //version
     router.post("/version",function(req,res){
